4531172e19936dfc6d6433d96acf7981ace11f3d
Thomas Turner <thomas_turner@talktalk.net>
2025-06-26 23:04:36 +0100

    kernel: Fake uname uprobestats
    5.10 bpf uses these features.

--- kernel/sys.c
+++ kernel/sys.c
@@ -1257,6 +1257,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 #ifdef CONFIG_BPF_SPOOF
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
+        !strncmp(current->comm, "uprobestats", 11) ||
 	    !strncmp(current->comm, "netd", 4)) {
 		strcpy(tmp.release, "5.4.186");
 		pr_debug("fake uname: %s/%d release=%s\n",
