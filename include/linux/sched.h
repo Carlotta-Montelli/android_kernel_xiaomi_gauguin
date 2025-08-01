5681d71e2430866feb9da6452e14491a41dd70db
Carlotta-Montelli <carlotta.montelli.0201@gmail.com>
2025-08-01 11:30:20 +0000

    Revert "BACKPORT: sched/headers: Move 'struct sched_param' out of uapi, to work around glibc/musl breakage"
    This reverts commit 6b157f48c1c59749a0fea3fa503b3cfbaf87352a.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -55,6 +55,7 @@ struct reclaim_state;
 struct capture_control;
 struct robust_list_head;
 struct sched_attr;
+struct sched_param;
 struct seq_file;
 struct sighand_struct;
 struct signal_struct;
@@ -369,10 +370,6 @@ enum uclamp_id {
 	UCLAMP_MIN = 0,
 	UCLAMP_MAX,
 	UCLAMP_CNT
-};
-
-struct sched_param {
-	int sched_priority;
 };
 
 struct sched_info {
