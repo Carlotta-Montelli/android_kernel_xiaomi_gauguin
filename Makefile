EmanuelCN/kernel_xiaomi_sm8250

Makefile: Pass more feature modifiers to -march • armv8.1-a: Includes armv8-a, +crc, +lse, +rdma by default. • fp16: Enables FP16 extension. This also enables floating-point instructions. • rcpc: Enables the RcPc extension. This is passed on to the assembler, 	enabling inline asm statements to use instructions from the RcPc extension.
Lookup: https://gcc.gnu.org/onlinedocs/gcc-10.1.0/gcc/AArch64-Options.html
Test: A successful build with both Clang & GCC. Kernel booted just fine.

Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

Makefile
@@ -684,8 +684,8 @@ KBUILD_CFLAGS   += -Os
 KBUILD_AFLAGS   += -Os
 KBUILD_LDFLAGS  += -Os
 else ifeq ($(cc-name),clang)
-KBUILD_CFLAGS   += -O3
-KBUILD_AFLAGS   += -O3
+KBUILD_CFLAGS   += -O3 -march=armv8.1-a+crypto+fp16+rcpc
+KBUILD_AFLAGS   += -O3 -march=armv8.1-a+crypto+fp16+rcpc
 KBUILD_LDFLAGS  += -O3
 else
 KBUILD_CFLAGS   += -O2

Carlotta-Montelli/android_kernel_xiaomi_gauguin

Makefile
@@ -717,9 +717,9 @@ KBUILD_CFLAGS	+= $(call cc-disable-warning, int-in-bool-context)
 KBUILD_CFLAGS	+= $(call cc-disable-warning, address-of-packed-member)
 
 ifdef CONFIG_CC_OPTIMIZE_FOR_SIZE
-KBUILD_CFLAGS   += -Os
+KBUILD_CFLAGS   += -Os -march=armv8.1-a+crypto+fp16+rcpc
 else
-KBUILD_CFLAGS   += -O2
+KBUILD_CFLAGS   += -O2 -march=armv8.1-a+crypto+fp16+rcpc
 endif
 
 ifdef CONFIG_CC_WERROR